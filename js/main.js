0c67ec203dfe32c457412eaef457839a41bf5b81
Milan Hundersmarck <milanhundersmarck01@hotmail.nl>
2020-05-29 10:02:44 +0200

    menu werk yes

--- js/main.js
+++ js/main.js
@@ -6,15 +6,26 @@ const menuBody = document.querySelector('#menubody');
 let menuStatus = false;
 
 menuButton.addEventListener('click', () => {
+    console.log(menuStatus);
     if (menuStatus == false) {
-        closeMenu.classList.remove('visible');
-        openMenu.classList.add('visible');
+        // closeMenu.classList.remove('visible');
+        // openMenu.classList.add('visible');
+        // menuBody.classList.add('change');
+        document.getElementById("menuLinkContainer").style.display = "block";
         menuBody.classList.add('change');
-        menuStatus = true;
-    } else if (menuStatus == true) {
         openMenu.classList.remove('visible');
         closeMenu.classList.add('visible');
-        menuButton.classList.remove('change');
+        menuStatus = true;
+    } else if (menuStatus == true) {
+        // openMenu.classList.remove('visible');
+        // closeMenu.classList.add('visible');
+        // menuButton.classList.remove('change');
+
+        menuBody.classList.remove('change');
+        closeMenu.classList.remove('visible');
+        openMenu.classList.add('visible');
         menuStatus = false;
+        document.getElementById("menuLinkContainer").style.display = "none";
     }
 });
+
