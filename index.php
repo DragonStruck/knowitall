a8f028ba670c1762ac3fb109300786b7845ea0fc
Milan Hundersmarck <milanhundersmarck01@hotmail.nl>
2020-06-04 11:53:53 +0200

    Willekeurig.php aangemaakt en menu links toegevoegd

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
     <div class="menubody" id="menubody">
         <div class="menuLinkContainer" id="menuLinkContainer">
             <a class="menuLinks menuactive" href="index.php" id="top">Home</a>
-            <a class="menuLinks" href="#">Willekeurig weetje</a>
+            <a class="menuLinks" href="willekeurig.php">Willekeurig weetje</a>
             <a class="menuLinks" href="#">Profiel</a>
             <a class="menuLinks" href="#">Weetje insturen</a>
         </div>
@@ -35,7 +35,7 @@
         <span class="logotekst">KnowItAll</span>
         <div class="topbarmenu">
             <a href="index.php">Home</a>
-            <a href="#">Willekeurig Weetje</a>
+            <a href="willekeurig.php">Willekeurig Weetje</a>
             <a href="#">Profiel</a>
         </div>
     </header>
